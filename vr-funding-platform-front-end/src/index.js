4426848bcb0f9196fc3d43c20813be618ea6214c
justin-mavity <justinmavity1021@gmail.com>
2021-01-07 16:50:55 -0600

    added rudecers

--- vr-funding-platform-front-end/src/index.js
+++ vr-funding-platform-front-end/src/index.js
@@ -1,20 +1,20 @@
-import './index.css';
-import App from './App';
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import { createStore, applyMiddleWare } from 'redux';
-// import { Provider } from 'react-redux';
-// import logger from 'redux-logger';
-// import thunk from 'redux-thunk';
-// import reducer from './store/reducers/postReducers';
-import { BrowserRouter as Router } from 'react-router-dom'
+import "./index.css";
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import PostReducer from "./store/reducers/PostReducers";
+import { BrowserRouter as Router } from "react-router-dom";
 
-// const store = createStore(reducer, applyMiddleWare(thunk, logger))
+const store = createStore(PostReducer, applyMiddleware(thunk, logger));
 ReactDOM.render(
   <Router>
-    {/* <Provider store={store}> */}
-    <App />
-    {/* </Provider> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
